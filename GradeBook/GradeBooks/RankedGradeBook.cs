86d126f2e00ca63dc963df69bd9057a8b4d54a9c
Gene C. Garber <gene.garber@gmail.com>
2020-10-04 20:57:09 -0400

    Add GetLetterGrade to the ranked grade book class which calculates the letter grade of where a given average grade falls into a percentile of all students' grades.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,8 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -10,6 +12,61 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            var grade = 'F';
+            var rankedGrades = getRankedGrades();
+            var calculatedPercentile = calculateGradePercentile(rankedGrades, averageGrade);
+
+            if (calculatedPercentile < 20)
+            {
+                grade = 'A';
+            }
+            else if (calculatedPercentile < 40)
+            {
+                grade = 'B';
+            }
+            else if (calculatedPercentile < 60)
+            {
+                grade = 'C';
+            }
+            else if (calculatedPercentile < 80)
+            {
+                grade = 'D';
+            }
+
+            return grade;
+        }
+
+        private double calculateGradePercentile(List<double> rankedGrades, double averageGrade)
+        {
+            var calculatedPercentile = (double)0;
+
+            for (var rankedGradeIndex = 0; rankedGradeIndex < rankedGrades.Count; rankedGradeIndex++)
+            {
+                if (averageGrade <= rankedGrades[rankedGradeIndex])
+                {
+                    calculatedPercentile = 100 - (double)((double)(rankedGradeIndex+1) / rankedGrades.Count * 100);
+                    break;
+                }
+            }
+
+            return calculatedPercentile;
+        }
+
+        private List<double> getRankedGrades()
+        {
+            var grades = Students.Select(student => student.AverageGrade).ToList();
+            grades.Sort();
+
+            return grades;
         }
     }
 }
